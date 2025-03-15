9531a11d0601892a4c4160538e42f49723a5c8d5
Benjaminr180 <benjaminrori180@gmail.com>
2025-03-15 14:38:28 -0600

    modified:   _001_Hola mundo.py

--- _001_Hola mundo.py
+++ _001_Hola mundo.py
@@ -1,4 +1,9 @@
 print("hola")
-#la funcion utilizada es para la impresion de una linea de texto
-print("Spiderman es el mejor super Heroe\npero tambien una gran persona")
-#el hecho de usar el \n es un salto de linea
+#La funcion utilizada es para la impresion de una linea de texto
+print("Spiderman es el mejor superhéroe\nPero tambien una gran persona")
+#El hecho de usar el \n es un salto de linea
+print("Spiderman es el mejor superhéroe",end=" ")
+print("Pero tambien una gran persona")
+#El end controla como sera la salida del siguiente print
+print("Spiderman","es","el","mejor","superhéroe",sep="_")
+#El sep funciona como un separador
