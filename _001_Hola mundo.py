4c483ab05fc4feec0cd6422d3fa854d2fe97099c
Benjaminr180 <benjaminrori180@gmail.com>
2025-03-15 14:15:10 -0600

    modified:   _001_Hola mundo.py

--- _001_Hola mundo.py
+++ _001_Hola mundo.py
@@ -1,1 +1,4 @@
 print("hola")
+#la funcion utilizada es para la impresion de una linea de texto
+print("Spiderman es el mejor super Heroe\npero tambien una gran persona")
+#el hecho de usar el \n es un salto de linea
