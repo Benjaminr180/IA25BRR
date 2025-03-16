e7323c6ce96ff1c023b6972be9a34d022d6994fb
Benjaminr180 <benjaminrori180@gmail.com>
2025-03-16 00:40:44 -0600

    new file:   _005_Variables.py

---
+++ _005_Variables.py
@@ -0,0 +1,10 @@
+variable = 'Messi'
+var = 10
+print("El dorsal y nombre del mejor jugador es: " +variable, var)
+var = var + 1
+print(var)
+a = 5.0
+b = 10.0
+c = (a ** 2 + b ** 2) ** 0.5
+print("c =", c)
+
