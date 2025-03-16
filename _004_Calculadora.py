656ae252c238d3635abe1cad3239e1e099c8cbe1
Benjaminr180 <benjaminrori180@gmail.com>
2025-03-16 00:20:45 -0600

    new file:   _004_Calculadora.py

---
+++ _004_Calculadora.py
@@ -0,0 +1,15 @@
+print("Estos son nuestros operadores conocidos: +","-","*","/","//","%","%","**",sep=", ")
+#Exponencial
+print(5 ** 4)
+#Multiplicación
+print(3 * 3)
+#División
+print(2 / 4)
+#División entera
+print( 2 // 4)#Solo imprime la parte entera
+#Residuo
+print( 10 % 4)#Es el valor que sobra después de dividir un valor entre otro para producir un resultado entero
+#Suma
+print(4 + 5)
+#Resta
+print(7 - 3)
