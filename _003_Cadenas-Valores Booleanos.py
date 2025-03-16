7d0c0ae32daa301040ee1f53236bdf775cb26a95
Benjaminr180 <benjaminrori180@gmail.com>
2025-03-15 23:43:45 -0600

    new file:   _003_Cadenas-Valores Booleanos.py

---
+++ _003_Cadenas-Valores Booleanos.py
@@ -0,0 +1,6 @@
+print("Me gusta\"ver la champions\"")#Aqui se muestra una cadena dentro de comillas siendo limitada por comillas
+print('Me gusta "ver la champions"')#Esto es otra forma con un apostrofe siendo ''<-- comilas simples y no dobles
+#Valores Booleanos
+#Estos se utilizan para verificar la veracidad donde se puede usar true o false denotados como 1 y 0
+print(True > False)
+print(True < False)
