220cfdad00ff97f123c857a69eb5ec9523255c8d
Benjaminr180 <benjaminrori180@gmail.com>
2025-03-15 23:15:32 -0600

    new file:   _002_Literales-Enteros y Flotantes.py

---
+++ _002_Literales-Enteros y Flotantes.py
@@ -0,0 +1,17 @@
+#Literales
+print("14")
+print(14)
+#ENTEROS
+print(55555555)#Esto es una forma de escrbir este numero sin separacion alguna pero puedes ser dificl de leer
+print(55_555_555)#Esto es otra forma de escribirlo con separacion de la forma correcta(si no se pone _ marca error )
+#B)En octonal y hexadecimal
+print("Nuestro numero octonal(0o170) en decimal es:")
+print(0o170)#Esta es la forma octonal para llamarla se hace anteponiendo a nuestro numero 0o y usaremos del 1-7 para 
+#poner nuestro valor octonal en nuestro caso octonal-->0o170=120<--- en decimal
+print("Nuestro numero hexadecimal(0x78) en decimal es:")
+print(0x78)
+#Flotantes
+print(9)#Esto python lo interpreta como un numero entero
+print(9.0)#Esto python lo interpreta como un numero punto-flotante aun asi sea .0 
+print(10e14)#Esto es una forma de poner un numero grande o chico sin necesidad de escribir muchos digitos
+print(.0000000000000000003)#Aunque escribamos el numero de esta forma en algunas ocaciones python lo interpretara como 3e-19
