fa6de95c14e94c82bb620cc8efe8f60f86bbba58
Benjaminr180 <benjaminrori180@gmail.com>
2025-03-16 15:26:46 -0600

    modified:   _006_Comentarios.py 	modified:   _007_Interaccion.py

--- _007_Interaccion.py
+++ _007_Interaccion.py
@@ -2,4 +2,4 @@ print("Dime el dorsal del mejor joven futbolista acutal")
 dorsal = input()
 print("Con el número:", dorsal,"Lamine Yamal!")
 Dorsal = input("Dime el dorsal de dinho en el F.C. Barcelona: ")
-print("Uso el dorsal: ", Dorsal)#gola
+print("Uso el dorsal: ", Dorsal)
