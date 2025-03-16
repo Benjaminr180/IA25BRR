0fcba68ea5a597a26fe0024436d773650588c619
Benjaminr180 <benjaminrori180@gmail.com>
2025-03-16 16:12:14 -0600

    modified:   _007_Interaccion.py

--- _007_Interaccion.py
+++ _007_Interaccion.py
@@ -3,3 +3,15 @@ dorsal = input()
 print("Con el número:", dorsal,"Lamine Yamal!")
 Dorsal = input("Dime el dorsal de dinho en el F.C. Barcelona: ")
 print("Uso el dorsal: ", Dorsal)
+#Conversión de tipos
+anything = float(input("Dame un numero aleatorio: "))
+potencia = anything ** 2
+print("LA potencia de tu numero ingresado: ",anything, "Es: ",potencia)
+#Operadores cadena
+primero = input("Nombre de la cadena de hamburguesas que tiene una M en su logo: ")
+segundo = input("Nombre de la cadena de hamburguesas que tiene una estrella en su logo: ")
+print("Dos cadenas de hamburguesas famosas son: " + primero + " y " + segundo)
+#Conversiones de tipos 2
+leg_a = float(input("Ingresa la longitud del primer cateto: "))
+leg_b = float(input("Ingresa la longitud del segundo cateto: "))
+print("La longitud de la hipotenusa es " + str((leg_a**2 + leg_b**2) ** .5))
